b67fb28bb87294c1355cb220bb698aab655bedde
Beke Ijobra <ijobra20@gmail.com>
2021-01-31 20:35:24 +0100

    some more update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
     return (
         <div className = {`nav ${show && 'nav_black'}`}>
             <img
-                src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+                src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png'
                 alt="Netflix logo"
                 className="nav_logo" />
             <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
